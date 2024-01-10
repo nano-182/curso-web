80e8d5bf698b52d3495545ac1a74131418745943
LuisOzuna <german.ozuna@cimagroup.com.mx>
2024-01-09 21:55:01 -0600

    tipo de datos

---
+++ 1_jsIntroduction/1_example.js
@@ -0,0 +1,18 @@
+//var, let, const
+const integer = 1;
+const float = 1.50;
+const string = "String";
+const boolean = true;
+const object = {
+  key: "value"
+};
+const array = ["array"];
+const nullValue = null;
+const undefinedValue = undefined;
+const symbol = Symbol();
+
+console.log(integer, float, string, boolean, object, array, nullValue, undefinedValue, symbol);
+console.log(typeof integer, typeof float, typeof string, typeof boolean, typeof object, typeof array, typeof nullValue, typeof undefinedValue, typeof symbol);
+
+
+console.log('test1:', "5" + 2 + 3)
